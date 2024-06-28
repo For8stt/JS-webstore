e07f7a051ed26c77b733a33ccfe7b21b62142b5b
For8stt <xkisil@stuba.sk>
2024-06-28 23:18:12 +0200

    Save cart in localStorage

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -2,6 +2,7 @@ import {cart, addToCart} from '../data/cart.js'
 import {products} from '../data/products.js'
 import {formatCurrency} from "./utils/money.js";
 
+updateCartQuantity();
 let productsHTML='';
 
 products.forEach((product)=> {
@@ -74,8 +75,10 @@ function updateCartQuantity(){
         cartQuantity+=item.quantity;
     });
 
-    document.querySelector('.js-cart-quantity')
-        .innerHTML=cartQuantity;
+    if (cartQuantity!==0) {
+        document.querySelector('.js-cart-quantity')
+            .innerHTML = cartQuantity;
+    }
 
 }
 
