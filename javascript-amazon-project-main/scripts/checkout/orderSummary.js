02737b78e71b1dc2af3ca39bf50e25f9f83b9b5a
For8stt <xkisil@stuba.sk>
2024-07-07 16:17:31 +0200

    Create automated tests for orderSummary

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -1,7 +1,6 @@
 import {cart, removeFromCart, updateDeliverOption} from '../../data/cart.js';
 import { getProduct} from '../../data/products.js';
 import {formatCurrency} from "../utils/money.js";
-import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliverOptions} from '../../data/deliverOptions.js';
 import {renderPaymentSummary} from "./paymentSummary.js";
@@ -27,6 +26,7 @@ export function renderOrderSummary() {
 
         cartSummaryHTML += `
        <div class="cart-item-container 
+            js-cart-item-container
             js-cart-item-container-${matchingProduct.id}">
                 
             <div class="delivery-date">
@@ -44,7 +44,8 @@ export function renderOrderSummary() {
                 <div class="product-price">
                   $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity
+                 js-product-quantity-${matchingProduct.id}">
                   <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
@@ -52,7 +53,7 @@ export function renderOrderSummary() {
                     Update
                   </span>
                   <span class="delete-quantity-link link-primary 
-                  js-delete-quantity-link" data-product-id="${matchingProduct.id}">
+                  js-delete-quantity-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
