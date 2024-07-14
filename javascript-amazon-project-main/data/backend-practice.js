233a371d3f4916326b561230393aa7e59e043e2f
For8stt <xkisil@stuba.sk>
2024-07-14 22:10:15 +0200

    Add backend practice file

---
+++ javascript-amazon-project-main/data/backend-practice.js
@@ -0,0 +1,10 @@
+const  xhr =new XMLHttpRequest();
+
+xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+})
+
+xhr.open('GET','https://supersimplebackend.dev');
+
+xhr.send();
+xhr.response
