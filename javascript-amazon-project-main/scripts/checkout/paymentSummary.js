943cc77488cc2ccd74a970d8656b64c9b0526f41
For8stt <xkisil@stuba.sk>
2024-07-18 12:26:06 +0200

    made intaractive tracking.html

--- javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -73,6 +73,7 @@ export function renderPaymentSummary() {
                 })
             });
             const order=await response.json();
+            console.log(order);
             addOrder(order);
         }catch (error){
             console.log('Unexpected error. Try again later.')
