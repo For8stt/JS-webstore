04f7fbfe059fcbe70f9862d77bdb23fd98e22c46
For8stt <xkisil@stuba.sk>
2024-07-18 13:51:35 +0200

    add new functions to amazon.js

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -17,6 +17,7 @@ export class Product{
   name;
   rating;
   priceCents;
+  keywords;
 
   constructor(productDetails) {
     this.id=productDetails.id;
@@ -24,6 +25,7 @@ export class Product{
     this.name=productDetails.name;
     this.rating=productDetails.rating;
     this.priceCents=productDetails.priceCents;
+    this.keywords=productDetails.keywords;
   }
 
   getStarsUrl(){
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -9,7 +9,25 @@ function renderProductsGrid() {
     updateCartQuantity();
     let productsHTML = '';
 
-    products.forEach((product) => {
+    const url=new URL(window.location.href);
+    const search=url.searchParams.get('search');
+
+    let filteredProducts = products;
+    if (search){
+        filteredProducts=products.filter((product)=>{
+            let matchingKeyword=false;
+
+            product.keywords.forEach((keyword)=>{
+               if (keyword.toLowerCase().includes(search.toLowerCase())){
+                   matchingKeyword=true;
+               }
+            });
+            return matchingKeyword || product.name.toLowerCase().includes(search.toLowerCase())
+        });
+    }
+
+
+    filteredProducts.forEach((product) => {
         productsHTML += `
        <div class="product-container">
           <div class="product-image-container">
@@ -75,6 +93,12 @@ function renderProductsGrid() {
             updateCartQuantity();
         })
     });
+    document.querySelector('.js-search-button')
+        .addEventListener('click',()=>{
+            const search =document.querySelector('.js-search-bar').value;
+            window.location.href=`amazon.html?search=${search}`;
+        })
+
 
     function updateCartQuantity() {
         let cartQuantity = 0;
